4f3374139487634a142d9c1a6022b1f314d2d83c
Macy Huston <mhuston@wifi-10-43-84-184.wifi.berkeley.edu>
2024-05-09 13:30:54 -0700

    small clarity improvements

--- tests/lumfuncs.py
+++ tests/lumfuncs.py
@@ -2,21 +2,23 @@
 Module containing methods to test models against 
 observed luminosity functions.
 Current options are:
-    compare_stanekwindow(mod_dict, area_mod)
+    compare_stanekwindow(model_data, model_area)
 """
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
     
-def compare_stanekwindow(mod_dict, area_mod):
+def compare_stanekwindow(model_data, model_area):
     """
     Module to compare a model catalog to the luminosity functions in the Stanek Window
-    from Terry et al. 2020.
+    (l,b = 0.25,-2.15) from Terry et al. 2020.
     inputs:
-        mod_dict: dictionary containing the following data arrays for the model catalog:
+        model_data: dictionary containing the following data arrays for the model catalog:
             {'V', 'I', 'J', 'H'} (apparent magnitudes)
-        area_mod: the size of the catalog region in degrees^2
+        model_area: the size of the catalog region in degrees^2
+    output:
+        a plot of the luminosity function histograms for each band
     """
     obs_data = pd.read_csv('data/stanekwindow_lumfuncs.txt', delim_whitespace=True, comment='#', na_values='-')
     filters = ['V','I','J','H']
@@ -30,7 +32,7 @@ def compare_stanekwindow(mod_dict, area_mod):
 
         bin_width=0.3
         mod_bins = np.array(obs_data[filt][~np.isnan(obs_data[filt])])
-        mod_hist = np.log10(np.histogram(mod_dict[filt], bins=mod_bins)[0]/ bin_width / area_mod / 60**2)
+        mod_hist = np.log10(np.histogram(model_data[filt], bins=mod_bins)[0]/ bin_width / model_area / 60**2)
         axs[j,k].step((mod_bins[1:]+mod_bins[:-1])/2, mod_hist, where='mid', label='model')
     axs[0,0].legend()
     plt.suptitle("Luminosity functions in Stanek Window\nl,b = 0.25,-2.15, data from Terry et al. 2020")
